d69b1e7555abddfbc8dba457be4d6c174661ed50
Deep Aghera <deep.aghera@marutitech.com>
2023-03-07 17:14:41 +0530

    some changes

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,5 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body,
+         Controller,
+         Get, 
+         Post, 
+         Req, 
+         Res,
+         Param, 
+         UsePipes, 
+         ValidationPipe, 
+         ParseIntPipe 
+        } from '@nestjs/common';
 
+        
+import { Request, Response } from 'express';
+import { CreateUserDto } from '../../dtos/CreateUser.dto';
 @Controller('users')
 export class UsersController {
     @Get()
@@ -9,4 +22,17 @@ export class UsersController {
             email : 'deepaghera001@gmail.com'
         }
     }
+
+    @Post()
+    @UsePipes(new ValidationPipe())
+    createUser(@Body() userData : CreateUserDto) {
+    console.log(userData);
+    return {};
+    }
+    @Get(':id')
+    getUserById(@Param('id',ParseIntPipe) id: string) {
+        console.log(id);
+        return {id};
+    }
+
 }
